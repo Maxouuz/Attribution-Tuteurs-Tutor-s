1be5c22e30c280a4098cd68021ea184879097057
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-04-02 09:36:47 +0200

    Update Person.java car final pas mis en majuscules

--- src/sae_101_02/Person.java
+++ src/sae_101_02/Person.java
@@ -1,16 +1,16 @@
 package sae_101_02;
 
 public abstract class Person {
-	private final String login;
-	private final String forename;
-	private final String name;
+	private final String LOGIN;
+	private final String FORENAME;
+	private final String NAME;
 	
-	public Person(String forename, String name, String extension) {
-		this.forename = forename;
-		this.name = name;
+	public Person(String FORENAME, String NAME, String extension) {
+		this.FORENAME = FORENAME;
+		this.NAME = NAME;
 		
 		// TODO: vérifier si le nom et prénom n'existe pas déjà!
-		String tmpLogin = forename + "." + name;
+		String tmpLogin = FORENAME + "." + NAME;
 		
 		// On peut ajouter une extension à la fin d'un login (comme .etu)
 		if (extension != null && !extension.equals(""))
@@ -19,8 +19,8 @@ public abstract class Person {
 		this.login = tmpLogin;
 	}
 	
-	public Person(String forename, String name) {
-		this(forename, name, null);
+	public Person(String FORENAME, String NAME) {
+		this(FORENAME, NAME, null);
 	}
 
 	public String getLogin() {
@@ -28,10 +28,10 @@ public abstract class Person {
 	}
 
 	public String getForename() {
-		return forename;
+		return FORENAME;
 	}
 
 	public String getName() {
-		return name;
+		return NAME;
 	}
 }
