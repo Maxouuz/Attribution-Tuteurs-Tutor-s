936fa8d456ba89b717d16064f1e0ecada266f1c9
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-04-02 09:39:13 +0200

    Update Teacher.java car final pas mis en majuscules

--- src/sae_101_02/Teacher.java
+++ src/sae_101_02/Teacher.java
@@ -6,8 +6,8 @@ import java.util.Collection;
 public class Teacher extends Person {
 	private ArrayList<Subject> subjectsTeached;
 	
-	public Teacher(String forename, String name, Collection<? extends Subject> subjectsTeached) {
-		super(forename, name);
+	public Teacher(String FORENAME, String NAME, Collection<? extends Subject> subjectsTeached) {
+		super(FORENAME, NAME);
 		this.subjectsTeached = new ArrayList<Subject>();
 		this.subjectsTeached.addAll(subjectsTeached);
 	}
