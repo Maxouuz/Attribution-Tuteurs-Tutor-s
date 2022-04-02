936973600f9eae934c16e043061b798dffe61246
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2022-04-02 09:40:48 +0200

    Update Tutor.java car final pas en majuscules

--- src/sae_101_02/Tutor.java
+++ src/sae_101_02/Tutor.java
@@ -3,9 +3,9 @@ package sae_101_02;
 public class Tutor extends Student {
 	private Subject subjectTeached;
 	
-	public Tutor(String forename, String name, double moyenne, int promo, Subject subjectTeached) {
+	public Tutor(String FORENAME, String NAME, double moyenne, int PROMO, Subject subjectTeached) {
 		// TODO: Prévoir le fait que la variable promo soit inférieur à 2 ou supérieur à 3
-		super(forename, name, moyenne, promo);
+		super(FORENAME, NAME, moyenne, PROMO);
 		this.subjectTeached = subjectTeached;
 	}
 
