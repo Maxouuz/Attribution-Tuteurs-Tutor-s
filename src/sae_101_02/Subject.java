bd50d7a8fa6fd995999d65f0e9e08addbd04cbfc
Remi Vautier <remi.vautier.etu@univ-lille.fr>
2022-04-06 09:12:58 +0200

    Update Subject.java

--- src/sae_101_02/Subject.java
+++ src/sae_101_02/Subject.java
@@ -1,7 +1,29 @@
 package sae_101_02;
 
+/**
+ *  Colors that can be used
+ *  {@link #R101}
+ *  {@link #R102}
+ *  {@link #R104}
+ *  {@link #R105}
+ */
 public enum Subject {
+	/**
+     * R101 Subject, Initiation au développement
+     */
 	R101("Initiation au développement");
+	/**
+     * R102 Subject, Interface Web
+     */
+	R102("Interface Web");
+	/**
+     * R104 Subject, Introduction aux systèmes d'exploitation et leur fonctionnement.
+     */
+	R104("Introduction aux systèmes d'exploitation et leur fonctionnement");
+	/**
+     * R105 Subject, Bases de données
+     */
+	R105("Bases de données");
 	
 	public final String NOM_MAT;
 	
